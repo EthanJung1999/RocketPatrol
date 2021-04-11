0065833b4734dcd4c025aee03b5b1b8b5572640c
Ethan Jung <ejung3@ucsc.edu>
2021-04-11 12:53:06 -0700

    Done

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -3,7 +3,7 @@ class Spaceship extends Phaser.GameObjects.Sprite{
         super(scene, x, y, texture, frame);
         scene.add.existing(this);   // add to existing scene
         this.points = pointValue;   // store pointValue
-        this.moveSpeed = 3;
+        this.moveSpeed = game.settings.spaceshipSpeed;
     }
 
     update(){
@@ -13,5 +13,10 @@ class Spaceship extends Phaser.GameObjects.Sprite{
         if(this.x <= 0 - this. width){
             this.x = game.config.width;
         }
+    }
+
+    //position reset
+    reset(){
+        this.x = game.config.width;
     }
 }
